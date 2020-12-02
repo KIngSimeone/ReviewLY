731d45a3739c681e96377fef19641058f98528d0
Kingsimeone <simeoneumoh@outlook.com>
2020-12-02 18:39:47 +0100

    Register user function added

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const mongoose= require('mongoose');
 const bodyparser=require('body-parser');
 const cookieParser=require('cookie-parser');
 const db=require('./config/config').get(process.env.NODE_ENV)
-
+const User=require('./models/user');
+const [auth]=require('./middlewares/auth');
 
 
 const app=express();
@@ -23,6 +24,31 @@ mongoose.connect(db.DATABASE,{ useNewUrlParser: true,useUnifiedTopology:true },f
 app.get('/',function(req,res){
     res.status(200).send(`Welcome to Reviewly`);
 });
+
+
+// adding new user (sign-up route)
+app.post('/api/register',function(req,res){
+    // collect user items from request body
+    const newuser=new User(req.body);
+    
+   if(newuser.password!=newuser.password2)return res.status(400).json({message: "password not match"});
+    
+    User.findOne({email:newuser.email},function(err,user){
+        if(user) return res.status(400).json({ auth : false, message :"email exits"});
+ 
+        newuser.save((err,doc)=>{
+            if(err) {console.log(err);
+                return res.status(400).json({ success : false});}
+            res.status(200).json({
+                succes:true,
+                user : doc
+            });
+        });
+    });
+ });
+
+
+
 
 // listening port
 const PORT=process.env.PORT||3000;
