84f94987e6eb7aca03520b5e0ab9dc3ede2c93bf
Kingsimeone <simeoneumoh@outlook.com>
2020-12-02 18:01:33 +0100

    Database connection setup

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const mongoose= require('mongoose');
 const bodyparser=require('body-parser');
 const cookieParser=require('cookie-parser');
+const db=require('./config/config').get(process.env.NODE_ENV)
+
+
 
 const app=express();
 // app use
@@ -9,8 +12,16 @@ app.use(bodyparser.urlencoded({extended : false}));
 app.use(bodyparser.json());
 app.use(cookieParser());
 
+// database connection
+mongoose.Promise=global.Promise;
+mongoose.connect(db.DATABASE,{ useNewUrlParser: true,useUnifiedTopology:true },function(err){
+    if(err) console.log(err);
+    console.log("database is connected");
+});
+
+
 app.get('/',function(req,res){
-    res.status(200).send(`Welcome to login , sign-up api`);
+    res.status(200).send(`Welcome to Reviewly`);
 });
 
 // listening port
