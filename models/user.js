75d4af7de25a5093a239139b0bbbd4045e9fba16
Kingsimeone <simeoneumoh@outlook.com>
2020-12-02 18:14:39 +0100

    Password hashing function added

--- models/user.js
+++ models/user.js
@@ -1,3 +1,7 @@
+const jwt=require('jsonwebtoken');
+const bcrypt=require('bcrypt');
+const confiq=require('../config/config').get(process.env.NODE_ENV);
+const salt=10;
 var mongoose=require('mongoose');
 
 const userSchema=mongoose.Schema({
@@ -30,6 +34,27 @@ const userSchema=mongoose.Schema({
     },
     token:{
         type: String
+    }
+});
+
+userSchema.pre('save',function(next){
+    var user=this;
+    
+    if(user.isModified('password')){
+        bcrypt.genSalt(salt,function(err,salt){
+            if(err)return next(err);
+
+            bcrypt.hash(user.password,salt,function(err,hash){
+                if(err) return next(err);
+                user.password=hash;
+                user.password2=hash;
+                next();
+            })
+
+        })
+    }
+    else{
+        next();
     }
 });
 
