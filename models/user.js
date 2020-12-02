5cfe6b7a2f09189867c2992410f1ddc41747543a
Kingsimeone <simeoneumoh@outlook.com>
2020-12-02 18:18:58 +0100

    Token functions added

--- models/user.js
+++ models/user.js
@@ -58,4 +58,54 @@ userSchema.pre('save',function(next){
     }
 });
 
+userSchema.methods.comparepassword=function(password,cb){
+    bcrypt.compare(password,this.password,function(err,isMatch){
+        if(err) return cb(next);
+        cb(null,isMatch);
+    });
+}
+
+// generate token
+
+userSchema.methods.generateToken=function(cb){
+    var user =this;
+    var token=jwt.sign(user._id.toHexString(),confiq.SECRET);
+
+    user.token=token;
+    user.save(function(err,user){
+        if(err) return cb(err);
+        cb(null,user);
+    })
+}
+
+// find user by token
+userSchema.statics.findByToken=function(token,cb){
+    var user=this;
+
+    jwt.verify(token,confiq.SECRET,function(err,decode){
+        user.findOne({"_id": decode, "token":token},function(err,user){
+            if(err) return cb(err);
+            cb(null,user);
+        })
+    })
+};
+
+
+//delete token
+
+userSchema.methods.deleteToken=function(token,cb){
+    var user=this;
+
+    user.update({$unset : {token :1}},function(err,user){
+        if(err) return cb(err);
+        cb(null,user);
+    })
+}
+
+
+
+
+
+
+
 module.exports=mongoose.model('User',userSchema);
